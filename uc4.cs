c46d0a843be99cf29ec810cb0836b4ec9287473b
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-14 17:19:32 +0530

    switch case

---
+++ uc4.cs
@@ -0,0 +1,43 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    public class EmpWageUsingCase4
+    {
+        public static void Case()
+        {
+
+            int empHrs = 0;
+            int empWage = 0;
+            int EMP_RATE_PER_HOUR = 20;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(3);
+
+            switch (empCheck)
+            {
+                case 0:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
+
+                case 1:
+                    Console.WriteLine("Employee is Present for Full Time");
+                    empHrs = 8;
+                    break;
+
+                case 2:
+                    Console.WriteLine("Employee is Present For Part Time");
+                    empHrs = 4;
+                    break;
+            }
+
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+
+            Console.WriteLine("Emp Wage :" + empWage);
+        }
+
+    }
+}
+
