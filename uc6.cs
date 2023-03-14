30f5a343ca532d00d70d47a7182c04cb469aa6f4
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-14 17:22:32 +0530

    working days and hours

---
+++ uc6.cs
@@ -0,0 +1,58 @@
+﻿namespace EmployeeWageComputataionProblem
+{
+    public class DaysCalculate
+    {
+        public static void MaxDaysHour()
+        {
+            int Max_Days = 20;
+            int Max_Hours = 100;
+
+
+            int Wage_Per_hrs = 20;
+            int EmpHrs = 0;
+
+            int Total_Wage = 0;
+
+
+            int Total_Work_Hour = 0;
+            int day = 1;
+
+
+            while (day <= Max_Days && Total_Work_Hour <= Max_Hours)
+            {
+                Random random = new Random();
+
+                int num = random.Next(3);
+
+                switch (num)
+                {
+                    case 0:
+                        Console.WriteLine("Employee Is Absent");
+                        EmpHrs = 0;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Employee Is Present For Full Time");
+                        EmpHrs = 8;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Employee Is Present For Part Time");
+                        EmpHrs = 4;
+                        break;
+
+                }
+                day++;
+
+                Total_Work_Hour = (Total_Work_Hour + EmpHrs);
+                Total_Wage = (Total_Work_Hour * Wage_Per_hrs);
+
+            }
+
+            Console.WriteLine("Total Work Hour Is " + Total_Work_Hour);
+            Console.WriteLine("Total Salary For A Month Is " + Total_Wage);
+
+
+        }
+    }
+}
