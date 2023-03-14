3a58f93db1d8f5928f9598de361a0914a53f3fc8
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-14 17:10:32 +0530

    employee wage problem

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeWageComputationProblem;
+
+namespace EmployeeWageComputataionProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation problem");
+            //EmployeeCheck.EmployeeAttendence();
+            //CalculateEmpWage.EmployeeWages();
+            //EmployeePartTimeWage.PartTimeEmployee();
+            //EmpWageUsingCase4.Case();
+             //WageForMonth5.WageFoMonth();
+            DaysCalculate.MaxDaysHour();
+        }
+    }
+}
